2d986bf10517373b64557f1ab5a3b6a6013b0223
Gremis <Gremis@DESKTOP-7T0JMV8>
2021-02-10 19:09:32 -0300

    App Product

---
+++ app.js
@@ -0,0 +1,27 @@
+class Product{
+    constructor(name, price, year){
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }
+}
+
+class UI {
+    addProduct(){
+
+    }
+
+    deleteProduct(){
+
+    }
+    
+    showMessage(){
+
+    }
+}
+
+//DOM Events
+
+document.getElementById('product-form').addEventListener('submit', function() {
+    alert ('Enviando formulario')
+})
